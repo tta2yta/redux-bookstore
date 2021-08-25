4f23a79bcb25c0a0246d4d4def69e3f51689e0ed
tta2yta <tta2yta@gmail.com>
2021-08-25 15:23:50 +0300

    Removing cached files

--- src/redux/book/Book.js
+++ src/redux/book/Book.js
@@ -1,31 +1,36 @@
 import { useState } from 'react';
+import AddRemove from '../../components/addRemove';
 
-const book = () => {
+const Book = () => {
   const [books] = useState([
     {
       id: Math.floor(Math.random() * 100),
       title: 'As a Man Thinketh',
       category: 'Self-Help',
+      author: 'AAA',
     },
     {
       id: Math.floor(Math.random() * 100),
       title: 'I, Robot ',
       category: 'Sci-Fic',
+      author: 'BBB',
     },
     {
       id: Math.floor(Math.random() * 100),
       title: 'Oliver Twist',
       category: 'Social Novel',
+      author: 'CCC',
     },
   ]);
 
   return (
     <div className="bookList">
       {books.map((item) => (
-        <div key={item.key}>
-          <div>{item.id}</div>
-          <div>{item.title}</div>
-          <div>{item.category}</div>
+        <div key={item.key} className="list-item">
+          <div className="book-category">{item.category}</div>
+          <div className="book-title">{item.title}</div>
+          <div className="book-author">{item.author}</div>
+          <AddRemove />
         </div>
 
       ))}
