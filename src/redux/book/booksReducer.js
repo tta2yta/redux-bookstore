7b771febd39c6e82b598310c1a6d614c91c0c2ac
tta2yta <tta2yta@gmail.com>
2021-08-26 01:30:22 +0300

    Add bookReducer

---
+++ src/redux/book/booksReducer.js
@@ -0,0 +1,12 @@
+const bookReducer=(state=initialState, action)=>{
+    switch(action.type){
+        case ADD_BOOK:
+            return state;
+        case REMOVE_BOOK:
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default bookReducer
