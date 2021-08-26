f194ab6fe7e765136b95f1e974cb3327dc01b492
tta2yta <tta2yta@gmail.com>
2021-08-26 11:43:39 +0300

    add Handlers to BookForm

--- src/components/books/BookForm.js
+++ src/components/books/BookForm.js
@@ -1,18 +1,39 @@
-const bookForm = () => (
-  <div className="add-book">
+import { useState } from 'react';
 
-    <form>
-      <input type="text" placeholder="Book Title" />
-      <select name="categories" id="categories">
-        <option value="fiction">Fiction</option>
-        <option value="scifiction">Science Fiction</option>
+const bookForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const submitBookToStore = () => {
+    const newBook = {
+      title,
+    };
+    console.log(newBook);
+  };
+  const handleOnChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
 
-        <option value="romance">Romance</option>
+  const handleOnSelect = (e) => {
+    setCategory(e.target.value);
+  };
+  return (
+    <div className="add-book">
+      <h2>ADD NEW BOOK</h2>
+      <form>
+        <input type="text" placeholder="Book Title" value={title} onChange={handleOnChange} />
+        <select name="categories" id="categories" onChange={handleOnSelect}>
+          <option value="fiction">Fiction</option>
+          <option value="scifiction">Science Fiction</option>
 
-        <option value="tech">Technology</option>
+          <option value="romance">Romance</option>
 
-      </select>
-    </form>
-  </div>
-);
+          <option value="tech">Technology</option>
+
+        </select>
+        <input type="button" value="ADD BOOk" onChange={submitBookToStore} />
+      </form>
+    </div>
+  );
+};
 export default bookForm;
