c106a2ee9e68f68a278dc630195d334a15177881
tta2yta <tta2yta@gmail.com>
2021-08-28 01:59:45 +0300

    Add action to get Api id

--- src/redux/book/actions/bookActions.js
+++ src/redux/book/actions/bookActions.js
@@ -1,6 +1,11 @@
+import axios from "axios";
+
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 const GET_BOOKS= 'GET_BOOKS'
+const GET_APIID= 'GET_APIID'
+
+
 export const addBook = (payLoad) => ({
   type: ADD_BOOK,
   payLoad,
@@ -28,5 +33,22 @@ export const fetchBooks=()=>{
       console.log("Unable to fetch data")
     }
 
+  }
+}
+
+export const getBooksApiId=()=>{
+  return (dispatch)=>{
+    const appId= axios.post(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/')
+      .then(res=> res.data)
+      if(appId){
+        dispatch({
+          type: GET_APIID,
+          payLoad:appId
+        })
+      }
+      else{
+        console.log('can not get api id')
+      }
   }
 }
