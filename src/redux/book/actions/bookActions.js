88a30a2e6e1d6feb0e072124c1b96ac83a19b735
tta2yta <tta2yta@gmail.com>
2021-08-28 01:45:06 +0300

    Add action getBooks

--- src/redux/book/actions/bookActions.js
+++ src/redux/book/actions/bookActions.js
@@ -1,5 +1,6 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const GET_BOOKS= 'GET_BOOKS'
 export const addBook = (payLoad) => ({
   type: ADD_BOOK,
   payLoad,
@@ -9,3 +10,23 @@ export const removeBook = (payLoad) => ({
   type: REMOVE_BOOK,
   payLoad,
 });
+
+export const fetchBooks=()=>{
+  return async (dispatch) =>{
+    const result= await fetch("",{
+      method:'GET',
+      headers:{'content-type': 'application/json'}
+    });
+    const response= await result.json()
+    if(json){
+      dispatch({
+        type:'GET_BOOKS',
+        payLoad:response
+      })
+
+    }else{
+      console.log("Unable to fetch data")
+    }
+
+  }
+}
